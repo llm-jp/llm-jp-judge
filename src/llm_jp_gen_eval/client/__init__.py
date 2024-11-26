afb4f03bb4708c44aaeae66b70e3369b6b172b01
k141303 <kouta.nakayama@gmail.com>
2024-11-26 11:49:05 +0900

    add Anthropic/Bedrock

--- src/llm_jp_gen_eval/client/__init__.py
+++ src/llm_jp_gen_eval/client/__init__.py
@@ -1,10 +1,12 @@
-from .remote import AzureOpenAI
+from .remote import AzureOpenAI, BedrockAnthropic
 from .local import vLLMClient
 
 
 def load_client(name="azure", **kwargs):
     if name == "azure":
         return AzureOpenAI(**kwargs)
+    elif name == "bedrock":
+        return BedrockAnthropic(**kwargs)
     elif name == "vllm":
         return vLLMClient(**kwargs)
     raise ValueError(f"Invalid client name: {name}")
--- src/llm_jp_gen_eval/client/remote.py
+++ src/llm_jp_gen_eval/client/remote.py
@@ -1,6 +1,7 @@
 import re
 import time
-from openai import AzureOpenAI as Client
+from openai import AzureOpenAI as AzureOpenAIClient
+from anthropic import AnthropicBedrock as AnthropicBedrockClient
 
 
 class AzureOpenAI:
@@ -19,7 +20,7 @@ class AzureOpenAI:
         self.max_retries = max_retries
         self.request_interval = request_interval
 
-        self.client = Client(
+        self.client = AzureOpenAIClient(
             api_key=api_key,
             api_version=api_version,
             azure_endpoint=api_endpoint,
@@ -63,3 +64,38 @@ class AzureOpenAI:
                 break
 
         return data
+
+
+class BedrockAnthropic(AzureOpenAI):
+    def __init__(
+        self,
+        model_name="anthropic.claude-3-5-sonnet-20240620-v1:0",
+        aws_access_key=None,
+        aws_secret_key=None,
+        aws_region="us-east-1",
+        max_tokens=128,
+        max_retries=1,
+        request_interval=1.0,
+    ):
+        self.model_name = model_name
+        self.max_tokens = max_tokens
+        self.max_retries = max_retries
+        self.request_interval = request_interval
+
+        self.client = AnthropicBedrockClient(
+            aws_access_key=aws_access_key,
+            aws_secret_key=aws_secret_key,
+            aws_region=aws_region,
+        )
+
+    def request(self, prompt, system_prompt=None):
+        messages = [{"role": "user", "content": prompt}]
+
+        completions = self.client.messages.create(
+            model=self.model_name,
+            messages=messages,
+            max_tokens=self.max_tokens,
+            system=system_prompt,
+        )
+
+        return completions.content[0].text
