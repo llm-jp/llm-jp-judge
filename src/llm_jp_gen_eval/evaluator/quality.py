0d7857fff5bad3f353bb9b374e91179b2f329757
k141303 <kouta.nakayama@gmail.com>
2024-12-12 17:13:19 +0900

    pattern matchのエラー率の修正

--- src/llm_jp_gen_eval/evaluator/quality.py
+++ src/llm_jp_gen_eval/evaluator/quality.py
@@ -105,8 +105,8 @@ class QualityEvaluator:
         pattern_match_errors = [
             raw_score["pattern"] is None for raw_score in raw_scores
         ]
-        pattern_match_error_rate = sum(pattern_match_errors) / len(
-            pattern_match_errors * 100
+        pattern_match_error_rate = (
+            sum(pattern_match_errors) / len(pattern_match_errors) * 100
         )
 
         logging.info(f"API error rate: {api_error_rate:.2f}%")
