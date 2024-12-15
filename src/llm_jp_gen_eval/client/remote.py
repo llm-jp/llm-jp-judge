2d6b4e4ec00acf2e9ecba162a3b9c8ae70442dc9
k141303 <kouta.nakayama@gmail.com>
2024-12-16 07:45:33 +0900

    multi turnに対応

--- src/llm_jp_gen_eval/client/remote.py
+++ src/llm_jp_gen_eval/client/remote.py
@@ -4,6 +4,8 @@ import logging
 import warnings
 import asyncio
 
+from copy import deepcopy
+
 import openai
 from openai import AzureOpenAI as AzureOpenAIClient
 from anthropic import AnthropicBedrock as AnthropicBedrockClient
@@ -47,28 +49,33 @@ class AzureOpenAI:
             azure_endpoint=api_endpoint,
         )
 
-    async def get_messages(self, prompt, system_prompt=None):
+    async def get_messages(self, prompt, response, system_prompt=None):
         if self.disable_system_prompt and system_prompt is not None:
-            prompt = f"{system_prompt}\n\n{prompt}"
+            prompt = deepcopy(prompt)
+            prompt[0] = f"{system_prompt}\n\n{prompt[0]}"
             system_prompt = None
 
-        if system_prompt is None:
-            messages = [{"role": "user", "content": prompt}]
-        else:
-            messages = [
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt},
-            ]
+        messages = []
+        for i in range(len(prompt)):
+            messages.append({"role": "user", "content": prompt[i]})
+            if i > 0:
+                messages.append({"role": "assistant", "content": response[i - 1]})
+
+        if system_prompt is not None:
+            messages.insert(0, {"role": "system", "content": system_prompt})
 
         return messages
 
     async def async_request(
         self,
         prompt,
+        response,
         system_prompt=None,
         sampling_params={},
     ):
-        messages = await self.get_messages(prompt, system_prompt=system_prompt)
+        messages = await self.get_messages(
+            prompt, response, system_prompt=system_prompt
+        )
 
         response = await asyncio.to_thread(
             self.client.chat.completions.create,
@@ -82,51 +89,81 @@ class AzureOpenAI:
         self, data, regex=None, system_prompt=None, sampling_params={}
     ):
         tasks = []
-        for i, d in enumerate(data):
+        wait = 0
+        for d in data:
+            if type(d["prompt"]) == str:  # Single turn
+                d["is_single_turn"] = True
+                d["prompt"] = [d["prompt"]]
+            elif type(d["prompt"]) == list:  # Multi turn
+                d["is_single_turn"] = False
+
             tasks.append(
                 self._process_single_request(
                     d,
                     regex,
                     system_prompt,
-                    wait=i * self.async_request_interval,
+                    wait=wait,
                     sampling_params=sampling_params,
                 )
             )
-        return await tqdm.asyncio.tqdm.gather(*tasks, desc=self.model_name)
+            wait += self.async_request_interval * len(d["prompt"])
+
+        data = await tqdm.asyncio.tqdm.gather(*tasks, desc=self.model_name)
+
+        for d in data:
+            is_single_turn = d.pop("is_single_turn")
+            if is_single_turn:
+                d["prompt"] = d["prompt"][0]
+                d["response"] = d["response"][0]
+                d["pattern"] = d["pattern"][0]
+                d["error_messages"] = d["error_messages"][0]
+
+        return data
 
     async def _process_single_request(
         self, d, regex, system_prompt, wait, sampling_params={}
     ):
         await asyncio.sleep(wait)
 
-        retry_count = 0
-        d["response"], d["pattern"], d["error_messages"] = None, None, []
-        while retry_count < self.max_retries:
-            try:
-                d["response"] = await self.async_request(
-                    d["prompt"],
-                    system_prompt=system_prompt,
-                    sampling_params=sampling_params,
-                )
-            except openai.RateLimitError as e:
-                d["error_messages"].append(str(e))
-                retry_count += 1
-                await asyncio.sleep(60)
-            except openai.BadRequestError as e:
-                d["error_messages"].append(str(e))
-                retry_count += 1
+        d["response"], d["pattern"], d["error_messages"] = [], [], []
+        for i in range(len(d["prompt"])):
+            retry_count = 0
+            # d["response"], d["pattern"], d["error_messages"] = None, None, []
+            response, pattern, error_messages = None, None, []
+            while retry_count < self.max_retries:
+                try:
+                    response = await self.async_request(
+                        d["prompt"][: i + 1],
+                        d["response"],
+                        system_prompt=system_prompt,
+                        sampling_params=sampling_params,
+                    )
+                except openai.RateLimitError as e:
+                    error_messages.append(str(e))
+                    retry_count += 1
+                    await asyncio.sleep(60)
+                except openai.BadRequestError as e:
+                    error_messages.append(str(e))
+                    retry_count += 1
+                    await asyncio.sleep(self.async_request_interval)
+                else:
+                    if regex is not None:
+                        m = re.search(regex, response)
+                        try:
+                            pattern = m.group(1)
+                        except (IndexError, AttributeError) as e:
+                            error_messages.append(str(e))
+                            retry_count += 1
+                            await asyncio.sleep(self.async_request_interval)
+                            continue
+                    break
+
+            d["response"].append(response)
+            d["pattern"].append(pattern)
+            d["error_messages"].append(error_messages)
+            if i < len(d["prompt"]) - 1:
                 await asyncio.sleep(self.async_request_interval)
-            else:
-                if regex is not None:
-                    m = re.search(regex, d["response"])
-                    try:
-                        d["pattern"] = m.group(1)
-                    except (IndexError, AttributeError) as e:
-                        d["error_messages"].append(str(e))
-                        retry_count += 1
-                        await asyncio.sleep(self.async_request_interval)
-                        continue
-                break
+
         return d
 
     def __call__(self, data, regex=None, system_prompt=None, sampling_params={}):
@@ -174,10 +211,11 @@ class BedrockAnthropic(AzureOpenAI):
     async def async_request(
         self,
         prompt,
+        response,
         system_prompt=None,
         sampling_params={},
     ):
-        messages = [{"role": "user", "content": prompt}]
+        messages = await self.get_messages(prompt, response)
 
         sampling_params = dict(sampling_params)
         # Ignore unsupported parameters
