b98c1fbf6f4cdbf391978ef1745d64ea9204d63a
k141303 <kouta.nakayama@gmail.com>
2025-01-09 01:30:15 +0900

    プロンプトの修正

--- src/llm_jp_gen_eval/evaluator/quality.py
+++ src/llm_jp_gen_eval/evaluator/quality.py
@@ -8,29 +8,32 @@ from collections import defaultdict
 from .base import BaseEvaluator
 
 PROMPT_TEMPLATE = """[指示]
-質問に対するAIアシスタントの回答を評価してください。
+質問に対するAIアシスタントの回答を以下の基準で評価してください。
 
-初めに、以下の各指標に基づいて回答を評価して下さい。
 正確性: 応答が事実を述べているか評価してください。虚偽や誤解を生む表現を含む応答には低い評価をつけてください。但し、創作や主観的な意見を求める質問の場合、この限りではありません。
 流暢性: 応答が自然な文章であるか評価してください。文法的に誤っている応答には低い評価をつけてください。
 詳細性: 応答が質問に対して十分な回答を提供しているか評価してください。回答が不足している場合は低い評価をつけてください。
 関連性: 応答が質問に関連しているか評価してください。質問と無関係な内容が含まれる場合は低い評価をつけてください。
+総合評価: 上記の基準を総合的に評価してください。
 
 評価値は1から5の間です。1は非常に悪く、5は非常に良いことを意味します。
-評価の理由を述べ、その後に評価値を記入してください。
-評価値は角括弧で囲み(例: [[3]])、以下の形式で記入してください。
+初めに評価の理由を述べ、その後に評価値を記入してください。
+評価値を二重角括弧で囲み(例: [[3]])、以下の形式で評価結果を記述してください。
 
-正確性: [[評価値]]
-流暢性: [[評価値]]
-詳細性: [[評価値]]
-関連性: [[評価値]]
+正確性(評価理由): 評価理由を記入
+正確性: [[評価値を記入]]
 
-次に、各指標の評価結果をもとに回答の総合評価を行ってください。
-評価値は1から5の間です。1は非常に悪く、5は非常に良いことを意味します。
-評価の理由を述べ、その後に評価値を記入してください。
-評価値は角括弧で囲み(例: [[3]])、以下の形式で記入してください。
+流暢性(評価理由): 評価理由を記入
+流暢性: [[評価値を記入]]
 
-総合評価: [[評価値]]
+詳細性(評価理由): 評価理由を記入
+詳細性: [[評価値を記入]]
+
+関連性(評価理由): 評価理由を記入
+関連性: [[評価値を記入]]
+
+総合評価(評価理由): 評価理由を記入
+総合評価: [[評価値を記入]]
 
 [質問]
 {question}
@@ -41,7 +44,7 @@ PROMPT_TEMPLATE = """[指示]
 
 METRICS = ["正確性", "流暢性", "詳細性", "関連性", "総合評価"]
 
-SCORE_REGEX = f"({'|'.join(METRICS)}): \[\[([1-5])\]\]"
+SCORE_REGEX = f"({'|'.join(METRICS)}):\s?\[\[([1-5])\]\]"
 
 
 class QualityScoreExtractor(object):
@@ -76,12 +79,16 @@ class QualityEvaluator(BaseEvaluator):
             "evaluation errors",
         ]
         for raw_output in raw_outputs:
+            if raw_output.get("pattern") is None:
+                scores = {metric: None for metric in METRICS}
+            else:
+                scores = [raw_output["pattern"].get(metric) for metric in METRICS]
             table.append(
                 [
                     raw_output["ID"],
                     raw_output["prompt"],
                     raw_output["response"],
-                    *[raw_output.get("pattern", {}).get(metric) for metric in METRICS],
+                    *scores,
                     json.dumps(raw_output["generate_errors"], ensure_ascii=False),
                     json.dumps(raw_output["error_messages"], ensure_ascii=False),
                 ]
